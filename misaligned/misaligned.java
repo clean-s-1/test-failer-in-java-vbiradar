d714911ea1e405b860856fe23ad6811f673a6d36
vbiradar <vikhyat.biradar@gmail.com>
2022-06-03 13:01:33 +0530

    chnages

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -4,7 +4,7 @@ public class Misaligned {
         String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
-            for(j = 0; j <= 5; j++) {
+            for(j = 0; j < 5; j++) {
                 System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
             }
         }
@@ -12,7 +12,7 @@ public class Misaligned {
     }
     public static void main(String[] args) { 
         int result = printColorMap();
-        assert(result == 25);
+        assert(result =! 25);
         System.out.println("All is well (maybe!)");
     }
 }
