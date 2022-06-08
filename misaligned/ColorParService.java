3a651794279f08a518f2af82fdcfd5f7e289defb
vbiradar <vikhyat.biradar@gmail.com>
2022-06-08 15:24:44 +0530

    color pair changes

---
+++ misaligned/ColorParService.java
@@ -0,0 +1,39 @@
+
+public class ColorParService {
+
+	public ColorPair getColorFromPairNumber(int pairNumber) {
+		int zeroBasedPairNumber = pairNumber - 1;
+
+		return new ColorPair();
+	}
+
+	public int getPairNumberFromColor() {
+		return 0;
+	}
+
+	public static int printColourMapAndreturnColorCodeNumber(String[] majorColors, String[] minorColors) {
+
+		System.out.println(+majorColors.length * minorColors.length + " pair color code");
+		System.out.println("Pair no|Major color|Minor color");
+		for (int i = 0; i < majorColors.length; i++) {
+			for (int j = 0; j < minorColors.length; j++) {
+
+				ColorPair colorPair = new ColorPair();
+				colorPair.setPairNumber(i * 5 + j + 1);
+				colorPair.setMajorColor(majorColors[i]);
+				colorPair.setMinorColor(minorColors[j]);
+				System.out.printf(getFormattedColorPair(colorPair));
+
+			}
+
+		}
+		return majorColors.length * minorColors.length;
+	}
+
+	public static String getFormattedColorPair(ColorPair colorPair) {
+
+		return String.format("%d %s %s %n", colorPair.getPairNumber(), colorPair.getMajorColor(),
+				colorPair.getMinorColor());
+	}
+
+}
