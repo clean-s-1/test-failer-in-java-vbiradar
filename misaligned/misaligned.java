58e0f695423c7b22130f4b1965defcd3ac42d411
vbiradar <vikhyat.biradar@gmail.com>
2022-06-09 10:02:00 +0530

    compilation changes

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,10 +1,10 @@
 public class Misaligned {
   
 	public static void main(String[] args) { 
-		
     	String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
         String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        int result = ColorParService.printColourMapAndreturnColorCodeNumber(majorColors, minorColors);
+        ColorPairService colorPairService = new ColorPairService();
+        int result = colorPairService.printColourMapAndreturnColorCodeNumber(majorColors, minorColors);
         
        assert(result == 25);
        System.out.println("All is well (maybe!)");
