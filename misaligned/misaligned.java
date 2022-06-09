6e5f50435e890ed4ba82c6362c3580598880d093
vbiradar <vikhyat.biradar@gmail.com>
2022-06-09 09:49:25 +0530

    compilation error

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,6 +1,7 @@
 public class Misaligned {
   
 	public static void main(String[] args) { 
+		
     	String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
         String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
         int result = ColorParService.printColourMapAndreturnColorCodeNumber(majorColors, minorColors);
